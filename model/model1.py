3844711b91ed537d0cdb6285878ee987e544d3da
sonyaku <ms.sonya.0@mail.ru>
2020-05-12 15:59:22 +0300

    first model

--- model/model1.py
+++ model/model1.py
@@ -5,32 +5,35 @@ import numpy as np
 #from metrics import hitrate
 from models.metrics import hitrate
 from sklearn.model_selection import train_test_split
+from models.product import getTopInCategory
 
 class model1():
 	def __init__(self, k = 5):
 		self.model = RandomForestClassifier()#criterion = ''
+		self.categories_ya = []
 		self.categories = []
 		self.criterion = {}
 		self.k = k
 		
-		df = pd.read_csv('C:/Users/msson/machine learning/курсовая/data/training_sample.csv')#, sep=';',encoding='cp1251')
-		
+		self.categories_ya = pd.read_csv('data/categories_with_yandex.csv')
+
+		df = pd.read_csv('data/training_sample.csv')#, sep=';',encoding='cp1251')
+
 		df = df.sample(frac=1)
-		df = df[:1000]
+		df = df[:10000]
 		
 		y_train = df.BU_Level4
 		self.categories = y_train.unique()
 		y_train = pd.get_dummies(y_train)
 
 		X_train = df.drop(['BU_Level4', 'ItemEAN'], axis = 1)
-		#print(df)
 		self.model.fit(X_train, y_train)
 		
-		X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-		pred = self.predict(X_test)
-		cat = self.get_categories(X_test)
-		self.criterion['hitrate'] = metrics.hitrate(pred, np.asarray(y_test))
-		self.criterion['diversity'] = metrics.diversity(cat)
+		#X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+		#pred = self.predict(X_test)
+		#cat = self.get_categories(X_test)
+		#self.criterion['hitrate'] = hitrate(pred, np.asarray(y_test))
+		#self.criterion['diversity'] = diversity(cat)
 		
 
 	def predict(self, X_test):
@@ -56,4 +59,19 @@ class model1():
 
 		return out
 		
-		
+	def get_gifts(self, X_test):
+		df = self.get_categories(X_test)
+		gifts = pd.DataFrame()
+		for i in df[0]:
+			a = np.asarray(self.categories_ya.loc[self.categories_ya['BU_Level4'] == i].yandex_category_id)[0]
+
+			try:
+				if (gifts.shape[0] != 0):
+					b = getTopInCategory(1, int(a), 1000, min_price=0, page=1) 
+					gifts = pd.concat([gifts, b], axis=0)
+				else:
+					gifts = getTopInCategory(1, int(a), 1000, min_price=0, page=1)
+				#arr0 = np.append(arr0, a)
+			except:
+				continue
+		return gifts	
